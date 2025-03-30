c8803eed9966488409190eca8019cf76cb820483
Marinos.Sake <protutor13@hotmail.com>
2025-03-30 20:37:14 +0300

    models schema

--- usersApp/app.js
+++ usersApp/app.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app = express();
 
+// app.get("/api/users", (req, resp) =>{
+
+// })
+
+// app.get("/api/users/user1", (req, resp) =>{
+
+// })
+
 module.exports = app
