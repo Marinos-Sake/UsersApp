d493b0c3f9f0bbc1dcfcfbf514ec12e4743fe2f7
Marinos.Sake <protutor13@hotmail.com>
2025-04-02 19:13:25 +0300

    a comment

--- usersApp/app.js
+++ usersApp/app.js
@@ -7,7 +7,7 @@ const user = require('./routes/user.routes');
 const userProduct = require('./routes/user.products.routes')
 const auth = require('./routes/auth.routes')
 
-app.use('/api/auth', auth)
+app.use('/api/auth', auth)  //auth --> κληρρωνομει οτι εχουμε στο αρχειο μας ./routes/auth.routes οπου κανουμε module.exports = router;
 app.use('/api/users', user)
 app.use('/api/user-product', userProduct)
 
