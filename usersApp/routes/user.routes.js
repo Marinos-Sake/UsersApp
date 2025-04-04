0b02e7275f5471bd25dcd58125a99c2993e93d05
Marinos.Sake <protutor13@hotmail.com>
2025-04-04 18:25:01 +0300

    swagger

--- usersApp/routes/user.routes.js
+++ usersApp/routes/user.routes.js
@@ -3,13 +3,14 @@ const express = require('express')
 const router = express.Router();
 
 const userController = require('../controllers/user.controller');
+const { verifyAccessToken } = require('../services/auth.service');
 const verifyToken = require('../middlewares/auth.middleware').verifyToken
 const verifyRoles = require('../middlewares/auth.middleware').verifyRoles
 
-router.get('/', userController.findALL);
+router.get('/', verifyToken, userController.findALL);
 router.get('/:username', userController.findOne)
 router.post('/', verifyToken, verifyRoles("ADMIN"), userController.create)
-router.patch('/:username', userController.update)
-router.delete('/:username', userController.deleteByUsername)
-router.delete('/:username/email/:email', userController.deleteByEmail)
+router.patch('/:username', verifyToken, verifyRoles("ADMIN"), userController.update)
+router.delete('/:username', verifyToken, verifyRoles("ADMIN"), userController.deleteByUsername)
+router.delete('/:username/email/:email', verifyToken, verifyRoles("ADMIN"), userController.deleteByEmail)
 module.exports = router;
