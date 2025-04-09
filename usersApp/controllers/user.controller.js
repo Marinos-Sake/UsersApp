82c03c057cd3db0f88a23f74a18f6c2a425f0d08
Marinos.Sake <protutor13@hotmail.com>
2025-04-09 21:05:28 +0300

    logger

--- usersApp/controllers/user.controller.js
+++ usersApp/controllers/user.controller.js
@@ -12,7 +12,8 @@ exports.findALL = async(req, resp) => {
         const result = await userService.findAll();
         resp.status(200).json({status: true, data: result});
         logger.info("Success in reading all users")
-        logger.warn("Success in reading all users")
+           logger.warn("Success in reading all users")
+           logger.error("Message with error")
     } catch(err) {
         console.log("Problem in reading users", err)
         logger.error("ERROR, Problem in reading all users", err)
--- usersApp/logger/logger.js
+++ usersApp/logger/logger.js
@@ -35,15 +35,56 @@
 
 
     //Third Example
-    requrire('winston-daily-rotate-file')
+    require('winston-daily-rotate-file')
+    require('winston-mongodb')
     const { format, createLogger, transports } = require('winston')
-    const { combine, timestamp, label, printf } = format
+    const { combine, timestamp, label, printf, prettyPrint } = format
     const CATEGORY = "Products app logs"
+
     const fileRotateTransport = new transports.DailyRotateFile({
         filename: "./logs/rotate-%DATE%.log",
         datePattern: "DD-MM-YYYY",
         maxFiles: "7d"
     })
 
+    const logger = createLogger({
+        format: combine(
+            label({label: "MY LABEL FOR PRODUCTS APP"}),
+            timestamp({format: "DD-MM-YYYY HH:mm:ss"}),
+            // format.json()
+            prettyPrint()
+        ),
+        
+        transports: [
+            new transports.Console(),
+            fileRotateTransport,
+            new transports.File(
+                {
+                    filename: "logs/example.log"
+                }
+            ),
+            new transports.File(
+                {
+                    level: "warn",
+                    filename: 'logs/warn.log'
+                }
+            ),
+            new transports.File(
+                {
+                    level: "info",
+                    filename: 'logs/info.log'
+                }
+            ),
+            new transports.MongoDB({
+                level: "warn",
+                db: process.env.MONGODB_URI,
+                collection: 'server_logs',
+                format: format.combine(
+                    format.timestamp(),
+                    format.json()
+                )
+            })
+        ]
+    })
 
     module.exports = logger;
