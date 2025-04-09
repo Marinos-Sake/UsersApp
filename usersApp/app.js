2ed6fcf8b51e14c59c100fccb20efb06af62f9d8
Marinos.Sake <protutor13@hotmail.com>
2025-04-09 23:53:18 +0300

    cors

--- usersApp/app.js
+++ usersApp/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./swagger')
@@ -10,9 +12,15 @@ const user = require('./routes/user.routes');
 const userProduct = require('./routes/user.products.routes')
 const auth = require('./routes/auth.routes')
 
+// app.use(cors({                          // cors = επιτρέπει επικοινωνία από άλλα domains.
+//     origin: ['http://localhost:5000'] //  πχ είναι απαραίτητο σε APIs που χρησιμοποιούνται από διαφορετικό frontend.
+// }))
+
 app.use('/api/auth', auth)  //auth --> μεταβλητή που κληρονομεί ό,τι έχουμε στο αρχείο μας ./routes/auth.routes οπου κάνουμε module.exports = router;
 app.use('/api/users', user)
 app.use('/api/user-product', userProduct)
+
+app.use('/', express.static('files'))
 
 app.use(
     '/api-docs',
