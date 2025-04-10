5a2f6ab0f45a6b5e1ec6fdee4b3cb34306fc8c70
Marinos.Sake <protutor13@hotmail.com>
2025-04-10 20:47:02 +0300

    url

--- usersApp/routes/auth.routes.js
+++ usersApp/routes/auth.routes.js
@@ -7,3 +7,5 @@ router.post('/login', authController.login);
 router.get('/google/callback', authController.googleLogin);
 
 module.exports = router;
+
+// https://accounts.google.com/o/oauth2/auth?client_id=568369441712-h3mq3kvhbul6nnrjuuv1dojus7ctkn9g.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
