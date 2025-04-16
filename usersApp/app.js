46179333b166acff4c12ab0c95e7eb44ad6ee63b
Marinos.Sake <protutor13@hotmail.com>
2025-04-16 18:01:59 +0300

    remove empty lines

--- usersApp/app.js
+++ usersApp/app.js
@@ -12,11 +12,8 @@ const user = require('./routes/user.routes');
 const userProduct = require('./routes/user.products.routes')
 const auth = require('./routes/auth.routes')
 
-// app.use(cors({                          // cors = επιτρέπει επικοινωνία από άλλα domains.
-//     origin: ['http://localhost:5000'] //  πχ είναι απαραίτητο σε APIs που χρησιμοποιούνται από διαφορετικό frontend.
-// }))
 
-app.use('/api/auth', auth)  //auth --> μεταβλητή που κληρονομεί ό,τι έχουμε στο αρχείο μας ./routes/auth.routes οπου κάνουμε module.exports = router;
+app.use('/api/auth', auth) 
 app.use('/api/users', user)
 app.use('/api/user-product', userProduct)
 
